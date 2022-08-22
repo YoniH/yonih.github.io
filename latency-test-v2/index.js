3f2f8acc88d595c216449badeaf3977ac8a7897e
Yoni Haimovich <yonihaimovich@Yonis-MacBook-Pro.local>
2022-08-22 16:27:51 +0300

    Add guitar

--- latency-test-v2/index.js
+++ latency-test-v2/index.js
@@ -2,6 +2,7 @@ const main = async () => {
   const kickSrc = "https://yonih.github.io/latency-test/samples/kick.wav";
   const snareSrc = "https://yonih.github.io/latency-test/samples/snare.wav";
   const hihatSrc = "https://yonih.github.io/latency-test/samples/hihat.wav";
+  const guitarSrc = "https://yonih.github.io/latency-test/samples/guitar.wav";
 
   const initBtn = document.getElementById("init");
   const descriptionDiv = document.getElementById("description");
@@ -13,10 +14,12 @@ const main = async () => {
   const tonePlayerKick = new Tone.Player(kickSrc).toDestination();
   const tonePlayerSnare = new Tone.Player(snareSrc).toDestination();
   const tonePlayerHihat = new Tone.Player(hihatSrc).toDestination();
+  const tonePlayerGuitar = new Tone.GrainPlayer(guitarSrc).toDestination();
 
   const toneGrainPlayerKick = new Tone.GrainPlayer(kickSrc).toDestination();
   const toneGrainPlayerSnare = new Tone.GrainPlayer(snareSrc).toDestination();
   const toneGrainPlayerHihat = new Tone.GrainPlayer(hihatSrc).toDestination();
+  const toneGrainPlayerGuitar = new Tone.GrainPlayer(guitarSrc).toDestination();
 
   initBtn.onclick = () => {
     Tone.start();
@@ -44,6 +47,7 @@ const main = async () => {
   const kickBuffer = await getAudioBuffer(kickSrc);
   const snareBuffer = await getAudioBuffer(snareSrc);
   const hihatBuffer = await getAudioBuffer(hihatSrc);
+  const guitarBuffer = await getAudioBuffer(guitarSrc);
 
   const playWebAudioApiSample = async (buffer) => {
     const sampleSource = audioContext.createBufferSource();
@@ -54,8 +58,8 @@ const main = async () => {
 
   ///
 
-  document.addEventListener("keydown", () => {
-    switch (event.key) {
+  document.addEventListener("keydown", (e) => {
+    switch (e.target.key) {
       case "e":
         tonePlayerKick.start();
         break;
@@ -64,6 +68,9 @@ const main = async () => {
         break;
       case "t":
         tonePlayerHihat.start();
+        break;
+      case "y":
+        tonePlayerGuitar.start();
         break;
       case "d":
         toneGrainPlayerKick.start();
@@ -74,6 +81,9 @@ const main = async () => {
       case "g":
         toneGrainPlayerHihat.start();
         break;
+      case "h":
+        toneGrainPlayerGuitar.start();
+        break;
       case "c":
         playWebAudioApiSample(kickBuffer);
         break;
@@ -82,6 +92,9 @@ const main = async () => {
         break;
       case "b":
         playWebAudioApiSample(hihatBuffer);
+        break;
+      case "n":
+        playWebAudioApiSample(guitarBuffer);
         break;
     }
   });
